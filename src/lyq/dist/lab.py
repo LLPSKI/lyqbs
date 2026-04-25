427896f60dfa9010c0090aa8e02c8740cf4ef4fa
YuQi Liu <992130522@qq.com>
2026-04-25 09:24:00 +0800

    lab实现位置更换

--- src/lyq/dist/lab.py
+++ src/lyq/dist/lab.py
@@ -7,7 +7,9 @@ import json
 import hashlib
 from functools import cached_property
 import time
+from pathlib import Path
 
+from tqdm import tqdm
 import torch
 import torch.distributed as dist
 from torch.distributed import (
@@ -17,11 +19,7 @@ from torch.nn.parallel.distributed import (
     DistributedDataParallel as DDP
 )
 from torch.optim import (
-    Optimizer,
     AdamW,
-)
-from torch.optim.lr_scheduler import (
-    LRScheduler,
 )
 
 from huggingface_hub import snapshot_download
@@ -38,14 +36,22 @@ from transformers.modeling_outputs import (
 )
 from datasets import load_dataset
 
-from .config import *
-from .log import *
-from .utils import *
-from ._lr_scheduler import *
-from ._comm_hooks import *
-from .dataset import LyqDataset
-from .dataloader import *
-from .trace import *
+from lyq import (
+    Configs,
+    Logger
+)
+from lyq.dist import *
+from lyq.dist.comm_hooks import *
+from lyq.utils.file import *
+from lyq.utils.data import (
+    LyqDataLoaderIterator,
+    LyqDataset,
+    BatchTokendSample
+)
+from lyq.utils.trace import (
+    TraceStepSchema
+)
+from lyq.optim import *
 
 __all__ = [
     'LyqLab',
@@ -79,10 +85,10 @@ class LyqLab():
         assert self._model_id_map.get(model), "实验的模型不存在！"
 
         self._model_id: str = self._model_id_map[model]
-        self._model_dir: str = output_dir() + self._model_id + '/'
+        self._model_dir: str = self.configs.output_dir + self._model_id + '/'
         self._main_dir: str = self._model_dir + 'main/'
 
-        info(
+        self.logger.info(
             f"处理模型参数中...\n"
             f"模型ID：{self._model_id}\n"
             f"模型目录路径：{self._model_dir}\n"
@@ -108,7 +114,7 @@ class LyqLab():
         self._optim_type: LyqLab.Optim = optim
         self._optim_configs: LyqLab.OptimSchema = optim_configs
 
-        info(
+        self.logger.info(
             f"处理优化器参数中...\n"
             f"优化器类型：{self._optim_type.name}\n"
             f"优化器配置：{self._optim_configs}"
@@ -132,7 +138,7 @@ class LyqLab():
         self._lr_type: LyqLab.LR = lr
         self._lr_configs: LyqLab.LRSchema = lr_configs
 
-        info(
+        self.logger.info(
             f"处理调度器参数中...\n"
             f"调度器类型：{self._lr_type.name}\n"
             f"调度器配置：{self._lr_configs}"
@@ -162,6 +168,8 @@ class LyqLab():
         assert self._quan_map.get(quan), "实验的量化钩子不存在！"
 
         self._quan: LyqLab.Quan = quan
+        if self._quan == LyqLab.Quan.NOQUAN:
+            self._commmetrix = noquan_get_commmetrix()
         self._comm_hook: Callable[
             [
                 dist.ProcessGroup,
@@ -170,7 +178,7 @@ class LyqLab():
             torch.futures.Future[torch.Tensor]
         ] = self._quan_map[quan]
 
-        info(
+        self.logger.info(
             f"处理量化参数中...\n"
             f"量化类型：{self._quan.name}\n"
             f"量化钩子：{self._comm_hook.__name__}"
@@ -189,7 +197,7 @@ class LyqLab():
         self._micro_num_per_batch = micro_num_per_batch
         self._micro_batch_size_per_device = micro_batch_size_per_device
 
-        info(
+        self.logger.info(
             f"处理训练参数中...\n"
             f"训练集单个样本最大长度：{self._max_seq_len}\n"
             f"训练总步数：{self._total_steps}\n"
@@ -208,7 +216,7 @@ class LyqLab():
         self._lab_desc_file: str = self._lab_dir + 'lab.json'
         self._lab_trace_file: str = self._lab_dir + 'trace.jsonl'
 
-        info(
+        self.logger.info(
             f"处理实验文件夹中...\n"
             f"实验文件夹路径：{self._lab_dir}\n"
             f"实验说明文件路径：{self._lab_desc_file}\n"
@@ -217,6 +225,8 @@ class LyqLab():
     
     def __init__(
         self,
+        configs: Configs,
+        logger: Logger,
         *,
         model: Model = Model.QWEN2_5_0_5_B,
         optim: Optim = Optim.ADAMW,
@@ -226,7 +236,7 @@ class LyqLab():
             "eps": 1e-8,
             "weight_decay": 0.01
         },
-        lr: LR = LR.LWLDLR,
+        lr: LR = LR.LWCDLR,
         lr_configs: LRSchema = {
             "warmup_ratio": 0.05,
             "min_lr_ratio": 0.05
@@ -237,6 +247,9 @@ class LyqLab():
         micro_num_per_batch: int = 32,
         micro_batch_size_per_device: int = 1
     ) -> None:
+        self.configs = configs
+        self.logger = logger
+
         self._check_model(model)
         self._check_optim(
             optim,
@@ -256,7 +269,7 @@ class LyqLab():
         self._check_lab()
         _register_lab(self)
 
-        info(
+        self.logger.info(
             f"实验对象处理完成！\n"
             f"实验ID：{self.id}"
         )
@@ -274,12 +287,12 @@ class LyqLab():
         """
         检查main模型
         """
-        info(f"检查main模型{self._model_id}文件中...")
+        self.logger.info(f"检查main模型{self._model_id}文件中...")
         if not is_files_all_exists(
             self._main_dir,
             self._main_files
         ):
-            info(
+            self.logger.info(
                 f"main模型{self._model_id}文件不完全！\n"
                  "从HF仓库中拉取..."
             )
@@ -289,7 +302,7 @@ class LyqLab():
                     "*.json"
                 ]
             )
-            info(
+            self.logger.info(
                 f"从HF仓库中拉取模型{self._model_id}完成！\n"
                  "创建初始原始模型并保存中..."
             )
@@ -315,19 +328,19 @@ class LyqLab():
                 save_original_format=False
             )
 
-            info(
+            self.logger.info(
                 f"原始模型{self._model_id}文件保存完成！\n"
                 f"保存路径：{self._main_files}"
             )
 
-        info(f"检查main模型{self._model_id}文件完成！")
+        self.logger.info(f"检查main模型{self._model_id}文件完成！")
     
     @master_function
     def _check_lab_dir(self) -> None:
-        info(f"检查实验{self.id}文件夹是否存在中...")
+        self.logger.info(f"检查实验{self.id}文件夹是否存在中...")
         # TODO lab.json文件不存在的情况
         if not Path(self._lab_dir).exists():
-            info(
+            self.logger.info(
                 f"实验{self.id}文件夹不存在！\n"
                 f"创建实验文件夹中..."
             )
@@ -335,7 +348,7 @@ class LyqLab():
             with Path(self._lab_desc_file).open("w", encoding='utf-8') as f:
                 f.write(self.__repr__())
             Path(self._lab_trace_file).touch()
-        info(f"检查实验{self.id}文件夹完成！")
+        self.logger.info(f"检查实验{self.id}文件夹完成！")
     
     _checkpoint_files: list[str] = [
         "config.json",
@@ -346,35 +359,35 @@ class LyqLab():
     ]
     
     def _init_where(self):
-        info("检查是否存在检查点文件中...")
-        p = find_checkpoint(self._lab_dir)
+        self.logger.info("检查是否存在检查点文件中...")
+        p = find_max_checkpoint(self._lab_dir)
         self._step: int = 0 # 记录模型执行的步数，0代表执行了0步，表明为初始状态
         if p is None:
-            info("没有发现检查点文件，使用main初始模型构建！")
+            self.logger.info("没有发现检查点文件，使用main初始模型构建！")
             self._where = self._main_dir
             self._step = 0
         else:
-            info(f"发现检查点{p.name}，检查有效性中...")
+            self.logger.info(f"发现检查点{p.name}，检查有效性中...")
             is_valid = True
             if not is_files_all_exists(str(p)+'/', self._checkpoint_files):
                 is_valid = False
             
             if is_valid:
-                info(f"检查点{p.name}有效，使用此检查点构建！")
+                self.logger.info(f"检查点{p.name}有效，使用此检查点构建！")
                 self._where = str(p)
                 self._step = int(p.name[-5:])
             else:
-                info(f"检查点{p.name}无效，使用main初始模型构建！")
+                self.logger.info(f"检查点{p.name}无效，使用main初始模型构建！")
                 self._where = self._main_dir
                 self._step = 0
         
-        info(f"检查{self._lab_trace_file}中的步数是否合法...")
+        self.logger.info(f"检查{self._lab_trace_file}中的步数是否合法...")
         with Path(self._lab_trace_file).open("r", encoding='utf-8') as f:
             line_count = sum(1 for _ in f)
         assert line_count == self._step, f"trace文件中步数{line_count}和希望的步数{self._step}不相符"
         if is_master():
             self._trace_writer = Path(self._lab_trace_file).open("a", encoding='utf-8')
-        info(
+        self.logger.info(
             f"检查检查点文件完成！\n"
             f"实验加载目录路径：{self._where}\n"
             f"当前实验已走步数：{self._step}\n"
@@ -387,6 +400,7 @@ class LyqLab():
             trust_remote_code=True
         )
         pretrain.to(self._device) # type: ignore
+        # pretrain.gradient_checkpointing_enable()
         self._model: DDP = DDP(
             pretrain,
             init_sync=False,
@@ -450,7 +464,7 @@ class LyqLab():
         self._lr_path = Path(self._where) / "lr.pth"
         self._warmup_steps = int(self._total_steps * self._lr_configs['warmup_ratio'])
         self._min_lr_ratio = self._lr_configs['min_lr_ratio']
-        info(
+        self.logger.info(
             f"基于{self._where}构建实验中...\n"
             f"从{self._where}加载模型：{self._model_id}\n"
             f"使用设备：{self._device}\n"
@@ -470,71 +484,38 @@ class LyqLab():
         self._load_lr()
     
     def _prepare_data(self):
-        info("准备数据集和数据分配器中...")
-        self._dataset = load_dataset(
-            path='json',
-            data_files={
-                "train": train_file(),
-                "valid": small_valid_file()
-            },
-            streaming=True
-        )
-        self._start = LyqDataLoader.step_to_start(
+        self.logger.info("准备数据集和数据分配器中...")
+        self._start = LyqDataLoaderIterator.step_to_start(
             self._step,
             self._micro_batch_size_per_device * self._micro_num_per_batch,
             self._world_size
         )
-        info(f"训练集跳过{self._start}条数据！")
-        self._train_dataset = self._dataset['train'].skip(self._start).map(
-            LyqDataset.process_sample,
-            remove_columns=[
-                'text',
-                'id',
-                'dump',
-                'date',
-                'file_path',
-                'language',
-                'language_score',
-                'score',
-                'int_score',
-                'url'
-            ],
-            fn_kwargs={
-                'tokenizer': self._tokenizer,
-                'max_len': self._max_seq_len
-            }
-        )
-        self._valid_dataset = self._dataset['valid']
+        self.logger.info(f"训练集跳过{self._start}条数据！")
         self._data_collator = DataCollatorForLanguageModeling(
             self._tokenizer,
             mlm=False
         )
-        self._train_dataloader = LyqDataLoader(
-            self._train_dataset,
-            self._data_collator,
-            self._micro_batch_size_per_device * self._micro_num_per_batch,
+        self._train_dataloader_iter = LyqDataLoaderIterator(
+            self.configs.train_file,
+            self._start,
             self._world_size,
             rank(),
+            self._micro_batch_size_per_device * self._micro_num_per_batch,
+            self._data_collator,
+            LyqDataset.process_sample,
+            self._tokenizer
         )
-        self._train_dataloader_iter: LyqDataLoaderIterator = iter(self._train_dataloader)
 
-        # self._valid_dataloader = LyqDataLoader(
-        #     self._train_dataset,
-        #     self._data_collator,
-        #     self._lab.micro_batch_size_per_device * self._lab.micro_num_per_batch,
-        #     start=0,
-        #     end=1024,
-        # )
-        info("训练数据加载器和验证数据加载器构建完成！")
+        self.logger.info("训练数据加载器和验证数据加载器构建完成！")
     
     def _prepare_for_train(self) -> None:
         """
         训练前准备工作
         """
-        info(
+        self.logger.info(
             "训练前准备工作进行中..."
         )
-        info(
+        self.logger.info(
             f"主进程开始检查模型以及实验文件！\n"
             f"其他进程barrier等待！"
         )
@@ -542,7 +523,7 @@ class LyqLab():
             self._check_main_model()
             self._check_lab_dir()
         
-        info(
+        self.logger.info(
             f"主进程检查完成！开始准备加载实验！"
         )
         self._init_where()
@@ -551,7 +532,7 @@ class LyqLab():
     
     def _pre_step(self) -> None:
         self._optim.zero_grad()
-        self.batch_data: BatchSchema = { # type: ignore
+        self.batch_data = { # type: ignore
             k: v.to(self._device) # type: ignore
             for k, v in next(self._train_dataloader_iter).items()
         } 
@@ -605,11 +586,11 @@ class LyqLab():
             self.train_loss,
             ReduceOp.AVG
         )
-        _commmetrix.all_reduce()
+        self._commmetrix.all_reduce()
 
         self._push_trace()
 
-        info(
+        self.logger.info(
             f"step: {self._step}\n"
             f"lr: {self.lr}\n"
             f"avg_loss: {self.train_loss.item()}"
@@ -622,10 +603,10 @@ class LyqLab():
             'step': self._step,
             'lr': self.lr,
             'train_time': self.train_time,
-            'grad_sync_total_time': _commmetrix.total_time,
-            'grad_sync_comp_time': _commmetrix.comp_time,
-            'grad_sync_comm_time': _commmetrix.comm_time,
-            'grad_sync_comm_bytes': _commmetrix.comm_bytes,
+            'grad_sync_total_time': self._commmetrix.total_time,
+            'grad_sync_comp_time': self._commmetrix.comp_time,
+            'grad_sync_comm_time': self._commmetrix.comm_time,
+            'grad_sync_comm_bytes': self._commmetrix.comm_bytes,
             'tokens': self.tokens.item(), # type: ignore
             'train_loss': self.train_loss.item(),
             'grad_norm': self.grad_norm.item(),
@@ -661,7 +642,6 @@ class LyqLab():
                     loss.backward()
             
             self.train_loss += loss
-        
         self._post_step()
     
     def train(
@@ -678,7 +658,7 @@ class LyqLab():
 
         self._prepare_for_train()
 
-        info(
+        self.logger.info(
             f"开始训练...\n"
             f"当前步数：{self._step}"
         )
@@ -688,40 +668,103 @@ class LyqLab():
             self._save_checkpoint()
     
     def _save_model(self):
-        info("保存模型中...")
+        self.logger.info("保存模型中...")
         self._model.module.save_pretrained(
             self._checkpoint_dir,
             save_peft_format=False,
             save_original_format=False
         )
-        info("保存模型完成！")
+        self.logger.info("保存模型完成！")
     
     def _save_optim(self):
-        info(f"保存优化器中...")
+        self.logger.info(f"保存优化器中...")
         torch.save(
             self._optim.state_dict(),
             self._checkpoint_dir + 'optim.pth'
         )
-        info(f"保存优化器完成！")
+        self.logger.info(f"保存优化器完成！")
     
     def _save_lr(self):
-        info(f"保存学习率调度器中...")
+        self.logger.info(f"保存学习率调度器中...")
         torch.save(
             self._lr.state_dict(),
             self._checkpoint_dir + 'lr.pth'
         )
-        info(f"保存学习率调度器完成！")
+        self.logger.info(f"保存学习率调度器完成！")
     
     def _save_checkpoint(self) -> None:
         with sync_scope():
             self._checkpoint_dir = self._lab_dir + 'checkpoint-' + f'{self._step:05d}/'
-            info(
+            self.logger.info(
                 f"保存检查点目录路径：{self._checkpoint_dir}..."
             )
             if is_master():
                 self._save_model()
                 self._save_optim()
                 self._save_lr()
+    
+    def verify(self) -> bool:
+        """
+        通过检查训练集中的词数量来检查训练是否有效
+        """
+        self.logger.info(
+            f"准备检查实验{self.id}有效性..."
+            f"准备分词器、数据集和数据分配器中..."
+        )
+        self._start = LyqDataLoaderIterator.step_to_start(
+            0,
+            self._micro_batch_size_per_device * self._micro_num_per_batch,
+            self._world_size
+        )
+        self._tokenizer: PreTrainedTokenizer = AutoTokenizer.from_pretrained(
+            self._main_dir,
+            trust_remote_code=True
+        )
+        self._data_collator = DataCollatorForLanguageModeling(
+            self._tokenizer,
+            mlm=False
+        )
+        self._train_dataloader_iter = LyqDataLoaderIterator(
+            self.configs.train_file,
+            self._start,
+            self._world_size,
+            rank(),
+            self._micro_batch_size_per_device * self._micro_num_per_batch,
+            self._data_collator,
+            LyqDataset.process_sample,
+            self._tokenizer
+        )
+        self.logger.info("分词器、数据集和数据分配器构建完成！")
+
+        self._trace_reader = Path(self._lab_trace_file).open("r", encoding='utf-8')
+
+        self.logger.info(
+            f"开始测试...\n"
+            f"如需看详细进度请看对应的日志文件{self.logger.log_file}"
+        )
+        is_valid = True
+        self._device: torch.device = device()
+        for i, line in enumerate(self._trace_reader):
+            trace_step: TraceStepSchema = json.loads(line)
+            tokens = trace_step['tokens']
+            batch_data: BatchTokendSample = next(self._train_dataloader_iter)
+            real_tokens = torch.sum(batch_data['token_count']).to(self._device)
+            dist.all_reduce(
+                real_tokens
+            )
+            self.logger.debug(
+                f"step: {i}: \n"
+                f"train_tokens = {tokens}\n"
+                f"real_tokens = {real_tokens.item()}"
+            )
+            if tokens != real_tokens.item():
+                self.logger.error(
+                    f"step: {i}: \n"
+                    f"train_tokens = {tokens}\n"
+                    f"real_tokens = {real_tokens.item()}"
+                )
+                is_valid = False
+        return is_valid
 
     def __repr__(self) -> str:
         return json.dumps(
@@ -752,25 +795,3 @@ def _register_lab(lab: LyqLab) -> None:
     global _current_lab
     assert not _current_lab, "目前只支持注册一个实验！"
     _current_lab.append(lab)
-
-# def set_grad_sync_total_time(grad_sync_total_time: float) -> None:
-#     global _current_lab
-#     assert not _current_lab, "目前没有实验注册！"
-#     _current_lab[0].grad_sync_total_time = grad_sync_total_time
-
-# def set_grad_sync_comp_time(grad_sync_comp_time: float) -> None:
-#     global _current_lab
-#     assert not _current_lab, "目前没有实验注册！"
-#     info(f"set_grad_sync_comp_time here!")
-#     _current_lab[0].grad_sync_comp_time = grad_sync_comp_time
-
-# def set_grad_sync_comm_time(grad_sync_comm_time: float) -> None:
-#     global _current_lab
-#     assert not _current_lab, "目前没有实验注册！"
-#     _current_lab[0].grad_sync_comm_time = grad_sync_comm_time
-
-# def set_grad_sync_comm_bytes(grad_sync_comm_bytes: int) -> None:
-#     global _current_lab
-#     assert not _current_lab, "目前没有实验注册！"
-#     info(f"set_grad_sync_comm_bytes here!")
-#     _current_lab[0].grad_sync_comm_bytes = grad_sync_comm_bytes
