8bdb031eb1b071f5b66fc063a22467b5df2b110f
YuQi Liu <992130522@qq.com>
2026-05-01 21:09:08 +0800

    新添加实验5：s1e1m6_110

---
+++ lab5.py
@@ -0,0 +1,29 @@
+from lyq import *
+from lyq.dist import *
+from lyq.dist.lab import *
+
+if __name__ == '__main__':
+    with global_env():
+        configs = Configs()
+        logger = Logger(
+            configs,
+            is_multirank=True,
+            rank=rank(),
+            is_master=is_master()
+        )
+        lab = LyqLab(
+            configs,
+            logger,
+            quan=LyqLab.Quan.S1E1M6_110_QUAN
+        )
+        
+        lab.train(
+            checkpoint_nums=4,
+            checkpoint_steps=10000
+        )
+
+        # 测试模型训练有效性需要较长的时间
+        # if not lab.verify():
+        #     logger.info("实验训练好像出了什么问题？！")
+        # else:
+        #     logger.info("实验训练没有问题！")
